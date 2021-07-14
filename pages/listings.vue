34418c8fb92752e5646195a6dcf577da2d2bb886
Irem Gokcek <gokcek.h.i@gmail.com>
2021-07-15 01:26:35 +0200

    main listing page designed with current and history listings

--- pages/listings.vue
+++ pages/listings.vue
@@ -1,7 +1,73 @@
-<template></template>
+<template>
+    <div class="all">
+        <div class="first">
+            <div>
+                <div class="title title text-3xl block"><h2>Active Listings</h2></div>
+            </div>
+
+            <div class="comment text-gray-500">
+                <h6>These listings are currently active and will be matched to the respective buyers/sellers at the end of the current Market Clock.</h6>
+            </div>
+
+            <br>
+    
+            <div class="activeListing">
+                <ActiveListingCard />
+            </div>
+        </div>
+
+        <div class="second">
+            <div>
+                <div class="title title text-3xl block"><h2>Matchings</h2></div>
+            </div>
+
+            <div class="comment text-gray-500">
+                <h6>These listings are not active, they have already been matched to the respective buyers/sellers in the previous Market Clocks.</h6>
+            </div>
+
+            <br>
+    
+            <div class="activeListing">
+                <HistoryListing />
+            </div>
+
+            <br><br><br><br><br>
+
+            <div class="activeListing">
+                <HistoryListing />
+            </div>
+
+            <br><br><br><br><br>
+
+            <div class="activeListing">
+                <HistoryListing />
+            </div>
+
+        </div>
+
+    </div>
+    
+</template>
 
 <script lang="ts">
 import Vue from "vue";
 
 export default Vue.extend({});
+
 </script>
+
+
+<style>
+
+.all{
+    padding:5%;
+}
+
+.first{
+    width:100%;
+}
+.second{
+    width:100%;
+    padding-top:10%;
+}
+</style>
