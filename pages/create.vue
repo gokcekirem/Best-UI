0b21ada6d74e26a77d56a72e4bb2fafbf2b9754b
cnbayraktaroglu <cnbayraktaroglu@gmail.com>
2021-07-15 16:42:24 +0200

    First round of facelift

--- pages/create.vue
+++ pages/create.vue
@@ -1,9 +1,29 @@
 <template>
-  <CreateListingCard />
+  <div class="page-container bg-ActiveAreaBackground">
+    <CreateListingCard />
+  </div>
 </template>
-
-<script lang="ts">
-import Vue from "vue";
-
-export default Vue.extend({});
+<style scoped>
+.page-container {
+  justify-content: center;
+  align-items: center;
+  display: flex;
+}
+</style>
+<script>
+import CreateListingCard from "../components/CreateListingCard";
+export default {
+  name: "Create",
+  head() {
+    return {
+      title: "BestMarket | Create",
+    };
+  },
+  components: {
+    CreateListingCard,
+  },
+  beforeMount() {
+    this.$currentPageSetter(this.$store, 0);
+  },
+};
 </script>
