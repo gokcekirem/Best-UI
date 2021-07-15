ac3cca499ab945688e15f7ed6917be3ea893c3df
Hatice Gökcek <ge59jec@mytum.de>
2021-07-15 21:34:49 +0200

    http get request for active listings implementation

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,7 +38,11 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxt/http'
   ],
+  http: {
+    // proxyHeaders: false
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
